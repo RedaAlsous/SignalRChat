f196b10a0de7170fe36869393abf424940ff127c
ASUS <relsous80@gmail.com>
2024-07-10 01:25:52 +0300

    Add project files.

---
+++ SignalRChat/Hubs/ChatHub.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.SignalR;
+namespace SignalRChat.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task sendMessage(string user, string message) 
+        {
+            await Clients.All.SendAsync("ReceiveMessage",user, message);
+        }
+    }
+}
